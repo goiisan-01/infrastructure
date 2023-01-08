2e13da59436c23b14e3e0e58007b3437d45046e1
goiisan-01 <training@goiisan.com>
2023-01-08 11:06:21 -0600

    deploy tf

--- Modules/Development/s3_module.tf
+++ Modules/Development/s3_module.tf
@@ -1,6 +1,6 @@
 module "s3" {
   source =  "../../Resources/s3"
 
-  bucket_name          = "dev-s3-bucket"
+  bucket_name          = "dev-s3-bucket-8989"
   env                  = "development"
 }
--- Modules/Production/s3_module.tf
+++ Modules/Production/s3_module.tf
@@ -1,6 +1,6 @@
 module "s3" {
   source =  "../../Resources/s3"
 
-  bucket_name          = "prd-s3-bucket"
+  bucket_name          = "prd-s3-bucket-8989"
   env                  = "production"
 }
