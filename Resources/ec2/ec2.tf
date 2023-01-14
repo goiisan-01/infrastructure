4e7714db592b8c1df71f6853386ec19a7df165b6
goiisan-01 <training@goiisan.com>
2023-01-14 11:26:01 -0600

    re-spin all env modules

--- Resources/ec2/ec2.tf
+++ Resources/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "main" {
   ami           = "ami-0b5eea76982371e91" 
   instance_type = var.instance_type
-  iam_instance_profile = aws_iam_instance_profile.profile.arn
+  iam_instance_profile = aws_iam_instance_profile.profile.name
   key_name = "new-key"
   #security_groups = [aws_security_group.main.id]
 
