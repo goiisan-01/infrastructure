04e06ebd8d532ac8352b32f8240812a98056e131
goiisan-01 <training@goiisan.com>
2023-01-14 11:17:13 -0600

    re-spin all env modules

--- Resources/ec2/ec2.tf
+++ Resources/ec2/ec2.tf
@@ -3,12 +3,12 @@ resource "aws_network_interface" "main" {
   subnet_id   = var.subnet_id
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "main" {
   ami           = "ami-0b5eea76982371e91" 
   instance_type = var.instance_type
   iam_instance_profile = aws_iam_instance_profile.profile.arn
   key_name = "new-key"
-  security_groups = aws_security_group.main.id
+  security_groups = [aws_security_group.main.id]
 
   network_interface {
     network_interface_id = aws_network_interface.main.id
