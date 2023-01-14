9d43c7554d7ea15f54a78c7202338f81b4cf8e71
goiisan-01 <training@goiisan.com>
2023-01-14 10:39:25 -0600

    re-spin all env modules

--- Resources/ec2/ec2.tf
+++ Resources/ec2/ec2.tf
@@ -1,7 +1,16 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0b5eea76982371e91"
+
+resource "aws_network_interface" "main" {
+  subnet_id   = module.vpc.subnet_id
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0b5eea76982371e91" 
   instance_type = var.instance_type
 
+  network_interface {
+    network_interface_id = aws_network_interface.main.id
+    device_index         = 0
+  }
   tags = {
     Name = var.ec2_name
   }
---
+++ Resources/vpc/outputs.tf
@@ -0,0 +1,4 @@
+output "subnet_id" {
+  type = string
+  default = aws_subnet.main.id
+}
