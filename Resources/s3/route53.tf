1957c311bdd29da1232d15df122ccad709e9c2ef
goiisan-01 <training@goiisan.com>
2023-01-21 11:00:51 -0600

    re-spin all env modules

--- Resources/s3/route53.tf
+++ Resources/s3/route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_record" "main" {
   type    = "A"
   ttl     = 300
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.zone_id
+    name    = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
   }
 }
