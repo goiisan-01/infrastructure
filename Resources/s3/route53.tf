5e083d9d31470b0fc6ee84094c2b56f24d97274a
goiisan-01 <training@goiisan.com>
2023-01-21 11:05:08 -0600

    re-spin all env modules

--- Resources/s3/route53.tf
+++ Resources/s3/route53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "main" {
   zone_id = "Z0527326Q1IRS4PX0WAW"
   name    = var.env_short
   type    = "A"
-  ttl     = 300
+
   alias {
     name    = aws_cloudfront_distribution.s3_distribution.domain_name
     zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
