e7f43e677d4c4417496698d26e8938013e4a1938
goiisan-01 <training@goiisan.com>
2023-01-21 10:41:51 -0600

    re-spin all env modules

--- Resources/s3/buckets.tf
+++ Resources/s3/buckets.tf
@@ -17,5 +17,4 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   error_document {
     key = "error.html"
   }
-
 }
---
+++ Resources/s3/cloudfront.tf
@@ -0,0 +1,45 @@
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.bucket.bucket_regional_domain_name
+    #origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = var.bucket_name
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  aliases = ["${var.env}.goiisan.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.bucket_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:505050218109:certificate/d1575fd6-7dd5-4ea5-9e36-df8aaf12dfa2"
+    ssl_support_method = "sni-only"
+}
+}
