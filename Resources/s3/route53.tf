7c9c1ca43188e168d9b7874ad2139f14c2f215be
goiisan-01 <training@goiisan.com>
2023-01-21 11:02:35 -0600

    re-spin all env modules

--- Resources/s3/route53.tf
+++ Resources/s3/route53.tf
@@ -6,5 +6,6 @@ resource "aws_route53_record" "main" {
   alias {
     name    = aws_cloudfront_distribution.s3_distribution.domain_name
     zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
   }
 }
