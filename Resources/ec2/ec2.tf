3393042100811a82bee377c63aca73aab41daf32
goiisan-01 <training@goiisan.com>
2023-01-28 10:54:03 -0600

    spin up infra

--- Resources/ec2/ec2.tf
+++ Resources/ec2/ec2.tf
@@ -1,22 +1,18 @@
-resource "aws_network_interface" "main" {
+/* resource "aws_network_interface" "main" {
   subnet_id   = var.subnet_id
 
   tags = {
     Name = var.ec2_name
   }
-}
+} */
 
 resource "aws_instance" "main" {
   ami           = "ami-0b5eea76982371e91" 
   instance_type = var.instance_type
   iam_instance_profile = aws_iam_instance_profile.profile.name
   key_name = "new-key"
+  subnet_id = var.subnet_id
   vpc_security_group_ids = [aws_security_group.main.id]
-
-  network_interface {
-    network_interface_id = aws_network_interface.main.id
-    device_index         = 0
-  }
 
   tags = {
     Name = var.ec2_name
