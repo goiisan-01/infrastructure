bf9edd53bc72ca999e276bdcc3bc41cc5584539d
goiisan-01 <training@goiisan.com>
2023-01-28 10:30:01 -0600

    spin up infra

--- Resources/ec2/ec2.tf
+++ Resources/ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "main" {
   instance_type = var.instance_type
   iam_instance_profile = aws_iam_instance_profile.profile.name
   key_name = "new-key"
-  #security_groups = [aws_security_group.main.id]
+  security_groups = [aws_security_group.main.id]
 
   tags = {
     Name = var.ec2_name
