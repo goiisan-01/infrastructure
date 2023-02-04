52ac54ca42f1c617bc8e528fcacbe58b2c526699
goiisan-01 <training@goiisan.com>
2023-02-04 10:27:44 -0600

    spin up infra

--- Resources/vpc/subnet.tf
+++ Resources/vpc/subnet.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidr_block
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = var.public_subnet_name
@@ -10,6 +11,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidr_block
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = var.private_subnet_name
