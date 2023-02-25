f0a1d18d44936aceb91d2384f7441e17296b0549
goiisan-01 <training@goiisan.com>
2023-02-25 10:38:23 -0600

    spin up asg

--- Resources/autoscaling/elb.tf
+++ Resources/autoscaling/elb.tf
@@ -14,8 +14,8 @@ resource "aws_route53_record" "main" {
   type    = "A"
 
   alias {
-    name    = aws_lb.main.domain_name
-    zone_id = aws_lb.main.hosted_zone_id
+    name    = aws_lb.main.dns_name
+    zone_id = aws_lb.main.zone_id
     evaluate_target_health = false
   }
 }
