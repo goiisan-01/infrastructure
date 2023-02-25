5c232aaa401931a0e51790a421571f8a0b4cc7ef
goiisan-01 <training@goiisan.com>
2023-02-25 10:23:53 -0600

    spin up asg

--- Modules/Production/autoscaling_module.tf
+++ Modules/Production/autoscaling_module.tf
@@ -5,7 +5,7 @@ module "autoscaling" {
   instance_type       = "t3.small"
   # iam_role            = "dev-private-iam-role"
   # iam_policy          = "dev-private-iam-policy"
-  sg_name             = "dev-private-security-group"
+  sg_name             = "dev-asg-security-group"
   # instance_profile    = "dev-private-instance-profile"
   # subnet_id           = module.vpc.private_subnet_id
   vpc_id              = module.vpc.vpc_id
