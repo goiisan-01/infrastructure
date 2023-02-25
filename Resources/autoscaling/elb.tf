8bef8c61721cc2b7a16838d933c262d1e8b019df
goiisan-01 <training@goiisan.com>
2023-02-25 11:07:32 -0600

    spin up asg

--- Resources/autoscaling/elb.tf
+++ Resources/autoscaling/elb.tf
@@ -8,6 +8,25 @@ resource "aws_lb" "main" {
   enable_deletion_protection = false
 }
 
+resource "aws_lb_target_group" "main" {
+  name     = var.ec2_name
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+
 resource "aws_route53_record" "main" {
   zone_id = "Z0527326Q1IRS4PX0WAW"
   name    = var.env_short
