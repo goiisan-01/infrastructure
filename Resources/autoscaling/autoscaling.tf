a70d68d808349efbefd97f42c400e168a7ba9da4
goiisan-01 <training@goiisan.com>
2023-03-11 10:38:31 -0600

    spin up infra

--- Resources/autoscaling/autoscaling.tf
+++ Resources/autoscaling/autoscaling.tf
@@ -14,6 +14,7 @@ resource "aws_autoscaling_group" "main" {
   max_size           = 10
   min_size           = 1
   target_group_arns = [aws_lb_target_group.main.arn]
+  vpc_zone_identifier       = [[var.subnet_id, var.subnet_id_2]]
 
   launch_template {
     id      = aws_launch_template.main.id
