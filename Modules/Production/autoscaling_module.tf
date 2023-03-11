c367f7c40554ffde8f9ee6fb1718cc35d1d6a931
goiisan-01 <training@goiisan.com>
2023-03-11 11:03:22 -0600

    spin up infra

--- Modules/Production/autoscaling_module.tf
+++ Modules/Production/autoscaling_module.tf
@@ -1,16 +1,16 @@
 module "autoscaling" {
   source =  "../../Resources/autoscaling"
 
-  ec2_name            = "prd-asg-private"
+  ec2_name            = "prd-asg-ec2"
   instance_type       = "t3.small"
-  iam_role            = "prd-asg-iam-role"
-  iam_policy          = "prd-asg-iam-policy"
-  sg_name             = "prd-asg-security-group"
-  instance_profile    = "prd-asg-instance-profile"
+  iam_role            = "prd-asg-role"
+  iam_policy          = "prd-asg-policy"
+  sg_name             = "prd-asg-ec2-security-group"
+  instance_profile    = "prd-asg-ec2-instance-profile"
   subnet_id           = module.vpc.public_subnet_id
   subnet_id_2         = module.vpc.public_subnet_id_2
   vpc_id              = module.vpc.vpc_id
   vpc_cidr            = module.vpc.vpc_cidr
   # associate_public_ip = false
-  env_short            = "prd-elb"
+  env_short            = "prd-asg-elb"
 }
--- Modules/Staging/autoscaling_module.tf
+++ Modules/Staging/autoscaling_module.tf
@@ -1,16 +1,16 @@
 module "autoscaling" {
   source =  "../../Resources/autoscaling"
 
-  ec2_name            = "stg-asg-private"
+  ec2_name            = "stg-asg-ec2"
   instance_type       = "t3.small"
-  iam_role            = "stg-asg-iam-role"
-  iam_policy          = "stg-asg-iam-policy"
-  sg_name             = "stg-asg-security-group"
-  instance_profile    = "stg-asg-instance-profile"
+  iam_role            = "stg-asg-role"
+  iam_policy          = "stg-asg-policy"
+  sg_name             = "stg-asg-ec2-security-group"
+  instance_profile    = "stg-asg-ec2-instance-profile"
   subnet_id           = module.vpc.public_subnet_id
   subnet_id_2         = module.vpc.public_subnet_id_2
   vpc_id              = module.vpc.vpc_id
   vpc_cidr            = module.vpc.vpc_cidr
   # associate_public_ip = false
-  env_short            = "stg-elb"
+  env_short            = "stg-asg-elb"
 }
