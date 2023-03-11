bf4b070762ffdcda7fc5729c385f81ebb7f37e23
goiisan-01 <training@goiisan.com>
2023-03-11 10:57:24 -0600

    spin up infra

--- Modules/Production/autoscaling_module.tf
+++ Modules/Production/autoscaling_module.tf
@@ -1,16 +1,16 @@
-# module "autoscaling" {
-#   source =  "../../Resources/autoscaling"
+module "autoscaling" {
+  source =  "../../Resources/autoscaling"
 
-#   ec2_name            = "prd-asg-private"
-#   instance_type       = "t3.small"
-#   iam_role            = "prd-asg-iam-role"
-#   iam_policy          = "prd-asg-iam-policy"
-#   sg_name             = "prd-asg-security-group"
-#   instance_profile    = "prd-asg-instance-profile"
-#   subnet_id           = module.vpc.public_subnet_id
-#   subnet_id_2         = module.vpc.public_subnet_id_2
-#   vpc_id              = module.vpc.vpc_id
-#   vpc_cidr            = module.vpc.vpc_cidr
-#   # associate_public_ip = false
-#   env_short            = "prd-elb"
-# }
+  ec2_name            = "prd-asg-private"
+  instance_type       = "t3.small"
+  iam_role            = "prd-asg-iam-role"
+  iam_policy          = "prd-asg-iam-policy"
+  sg_name             = "prd-asg-security-group"
+  instance_profile    = "prd-asg-instance-profile"
+  subnet_id           = module.vpc.public_subnet_id
+  subnet_id_2         = module.vpc.public_subnet_id_2
+  vpc_id              = module.vpc.vpc_id
+  vpc_cidr            = module.vpc.vpc_cidr
+  # associate_public_ip = false
+  env_short            = "prd-elb"
+}
