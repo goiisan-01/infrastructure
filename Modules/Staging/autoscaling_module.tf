bac39824b2ddbcf4fb3f845b36fe59de930e8703
goiisan-01 <training@goiisan.com>
2023-03-11 10:23:31 -0600

    spin up infra

--- Modules/Staging/autoscaling_module.tf
+++ Modules/Staging/autoscaling_module.tf
@@ -1,16 +1,16 @@
-module "autoscaling" {
-  source =  "../../Resources/autoscaling"
+# module "autoscaling" {
+#   source =  "../../Resources/autoscaling"
 
-  ec2_name            = "stg-asg-private"
-  instance_type       = "t3.small"
-  iam_role            = "stg-asg-iam-role"
-  iam_policy          = "stg-asg-iam-policy"
-  sg_name             = "stg-asg-security-group"
-  instance_profile    = "stg-asg-instance-profile"
-  subnet_id           = module.vpc.public_subnet_id
-  subnet_id_2         = module.vpc.public_subnet_id_2
-  vpc_id              = module.vpc.vpc_id
-  vpc_cidr            = module.vpc.vpc_cidr
-  # associate_public_ip = false
-  env_short            = "stg-elb"
-}
+#   ec2_name            = "stg-asg-private"
+#   instance_type       = "t3.small"
+#   iam_role            = "stg-asg-iam-role"
+#   iam_policy          = "stg-asg-iam-policy"
+#   sg_name             = "stg-asg-security-group"
+#   instance_profile    = "stg-asg-instance-profile"
+#   subnet_id           = module.vpc.public_subnet_id
+#   subnet_id_2         = module.vpc.public_subnet_id_2
+#   vpc_id              = module.vpc.vpc_id
+#   vpc_cidr            = module.vpc.vpc_cidr
+#   # associate_public_ip = false
+#   env_short            = "stg-elb"
+# }
