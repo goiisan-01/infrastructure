f91ffd3a8c8b2c5aea912926c97fa9c1016e03dd
goiisan-01 <training@goiisan.com>
2023-03-11 10:29:15 -0600

    spin up infra

--- Resources/autoscaling/autoscaling.tf
+++ Resources/autoscaling/autoscaling.tf
@@ -19,6 +19,11 @@ resource "aws_autoscaling_group" "main" {
     id      = aws_launch_template.main.id
     version = "$Latest"
   }
+  tag {
+    key                 = "Name"
+    value               = var.ec2_name
+    propagate_at_launch = true
+  }
   depends_on = [aws_lb.main]
 }
 
